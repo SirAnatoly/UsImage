778909c426859257b545e5313e05a93a2d9a200c
MrAnatoliy <kovalv97@gmail.com>
2021-02-15 09:18:54 +0200

    Added classes for converting strings into English

---
+++ usimage-model/src/main/java/com/siranatoliy/usimage/model/domain/AbstractDomain.java
@@ -0,0 +1,68 @@
+/* 
+ * Copyright 2021 Anatoliy Kovalv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siranatoliy.usimage.model.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * Abstract class for storing the common of the model classes
+ * 
+ * @author Anatoliy Kovalv
+ * @version 1.0 
+ */ 
+@MappedSuperclass
+public abstract class AbstractDomain implements Serializable{
+
+    @NotNull
+    @Past
+    @Basic(optional = false)
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
---
+++ usimage-model/src/main/java/com/siranatoliy/usimage/model/validation/EnglishLanguageConstraintValidator.java
@@ -0,0 +1,77 @@
+/* 
+ * Copyright 2021 Anatoliy Kovalv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.siranatoliy.usimage.model.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+/**
+ * Class for converting strings to English
+ * 
+ * @author Anatoliy Kovalv
+ * @version 1.0 
+ */  
+public class EnglishLanguageConstraintValidator implements ConstraintValidator<EnglishLanguage, String> {
+
+    private boolean withNumbers;
+    private boolean withPunctuations;
+    private boolean withSpecialSymbols;
+
+    @Override
+    public void initialize(EnglishLanguage constraintAnnotation) {
+        this.withNumbers = constraintAnnotation.withNumbers();
+        this.withPunctuations = constraintAnnotation.withPunctuations();
+        this.withSpecialSymbols = constraintAnnotation.withSpecialSymbols();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        String validationTemplate = getValidationTemplate();
+        for (int i = 0; i < value.length(); i++) {
+            char ch = value.charAt(i);
+            if (validationTemplate.indexOf(ch) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static final String SPECIAL_SYMBOLS = "~#$%^&*-+=_\\|/@`!'\";:><,.?{}";
+    private static final String PUNCTUATIONS = ".,?!-:()'\"[]{}; \t\n";
+    private static final String NUMBERS = "0123456789";
+    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private String getValidationTemplate() {
+        StringBuilder template = new StringBuilder(LETTERS);
+        if (withNumbers) {
+            template.append(NUMBERS);
+        }
+        if (withPunctuations) {
+            template.append(PUNCTUATIONS);
+        }
+        if (withSpecialSymbols) {
+            template.append(SPECIAL_SYMBOLS);
+        }
+        return template.toString();
+    }
+}
+
+
